d0d0ddfb030b9f80137f3ace27d4bece82bd6e1f
Matt Perry <mpcomplete@gmail.com>
2026-07-08 12:23:25 -0400

    Update 2.1

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-name=LogisticConfigurator_1.1.2
-./zip.sh
-mv ${name}.zip 'C:\Users\mpcom\AppData\Roaming\Factorio\mods'
-start "steam://rungameid/427520"
-#"C:\Program Files (x86)\Steam\steamapps\common\Factorio\bin\x64\factorio.exe"
-#"C:\Program Files\Factorio\bin\x64\factorio.exe"
-#echo cmd /c '"C:\Program Files\Factorio\bin\x64\factorio.exe"'
-#cmd /c "C:\\Program Files\\Factorio\\bin\\x64\\factorio.exe"
--- zip.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-name=LogisticConfigurator_1.1.2
-mkdir $name
-cp -a * $name
-rm $name/*.sh $name/*.zip $name/$name $name/*.png -rf
-cp thumbnail.png $name
-rm -rf ${name}.zip
-7z a ${name}.zip $name
-rm -rf $name
